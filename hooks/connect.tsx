0c6f205b3725b0a8e2effd40794936ddedf40764
owner <>
2022-11-28 18:32:53 +0900

    deploy

--- hooks/connect.tsx
+++ hooks/connect.tsx
@@ -19,7 +19,8 @@ type Props = {
 // コントラクトとの接続を行うための関数
 export const connectToContract = async (props: Props) => {
   // rpcのURL
-  const blockchainUrl = "ws://127.0.0.1:9944";
+  // const blockchainUrl = "ws://127.0.0.1:9944";
+  const blockchainUrl = "Blast: wss://shibuya.public.blastapi.io";
 
   // この関数でアカウント情報を取得する
   const extensionSetup = async () => {
